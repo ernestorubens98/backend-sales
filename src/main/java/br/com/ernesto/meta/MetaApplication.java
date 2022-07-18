2231dc9646ff14bfab35c4f4d8e44d547edfc683
Ernesto Rubens <ernestorubens13@gmail.com>
2022-07-17 22:04:19 -0300

    Concluido desenvolvimento com deploy na aws

--- src/main/java/br/com/ernesto/meta/MetaApplication.java
+++ src/main/java/br/com/ernesto/meta/MetaApplication.java
@@ -3,7 +3,6 @@ package br.com.ernesto.meta;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
 @EnableSwagger2
